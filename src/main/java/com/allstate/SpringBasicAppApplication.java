64a1ee477d382c78099fc47abff64fb6d56c2579
ghubl <ghubl@allstate.com>
2017-01-11 10:08:10 +0530

    first commit

---
+++ src/main/java/com/allstate/SpringBasicAppApplication.java
@@ -0,0 +1,12 @@
+package com.allstate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBasicAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBasicAppApplication.class, args);
+	}
+}
